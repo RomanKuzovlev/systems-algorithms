4a51770d5e16cc402de20513d416f7d43ec02fe6
RomanKuzovlev <ro.kuzovlev@gmail.com>
2025-06-25 21:56:20 +0200

    load len and array

--- asm/sorting.s
+++ asm/sorting.s
@@ -8,9 +8,13 @@ len:
 .section .text
 
 _start:
-   
+    lea array(%rip), %rsi      # %rsi = pointer to array
+    mov len(%rip), %ecx        # %ecx = len (5)
 
     # Exit syscall (Linux x86-64)
     mov $60, %rax         # syscall number for exit
     xor %rdi, %rdi        # exit code 0
     syscall
+
+
+// movl - move double word
