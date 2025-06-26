ddf43309e20e0ab0529c2f4c7668229e5935cb91
RomanKuzovlev <ro.kuzovlev@gmail.com>
2025-06-26 19:53:27 +0200

    added swapped flag

--- asm/sorting.s
+++ asm/sorting.s
@@ -1,3 +1,7 @@
+.section .bss
+swapped:
+    .skip 1                  # 1 byte for the swapped flag
+
 .section .data
 array:
     .long 5, 3, 8, 1, 2      # 5 ints array
@@ -11,10 +15,12 @@ _start:
     lea array(%rip), %rsi      # %rsi = pointer to array
     mov len(%rip), %ecx        # %ecx = len (5)
 
+    movb $1, swapped(%rip)
+
     # Exit syscall (Linux x86-64)
     mov $60, %rax         # syscall number for exit
     xor %rdi, %rdi        # exit code 0
     syscall
 
 
-// movl - move double word
+
